c48e465a5e315c7df5c82e19bb300343314961e3
Joab Nyabuto <joabonyabuto@gmail.com>
2026-07-25 18:59:15 -0500

    testing app deployment

--- CampusTradeTTT-main/Login_Controller.php
+++ CampusTradeTTT-main/Login_Controller.php
@@ -1,45 +1,4 @@
 <?php
-/*
-    session_start();
-    $db = require __DIR__ . '/Database.php'; 
-    require __DIR__ . '/UserModel.php';
-
-    $userModel = new UserModel($db);
-
-    //write your code here
-
-    if($_SERVER['REQUEST_METHOD']=== 'POST'){
-
-        if(empty($_POST['email']) || empty($_POST['password'])){
-            $msg = "Email and password fields can't be empty";
-            $_SESSION['flash_errors'] = [$msg];
-            $_SESSION['old'] = $_POST;
-            header('Location: LoginPage.php');
-        }
-
-        try{
-
-            $User_email = $_POST['email'];    
-            $User_password = $_POST['password'];
-
-            $user = $userModel->VerifyUser($User_email,$User_password);
-
-            header('Location: buyerpage.php');
-            exit;
-
-        }catch(InvalidArgumentException $e){
-            $_SESSION['flash_errors'] = [$e ->getmessage()];
-            $_SESSION['old'] = $_POST;
-
-            header('Location: LoginPage.php');
-            exit;
-        }catch(RuntimeException $e){
-            $_SESSION['flash_errors'] = ['Server error'];
-            header('Location: LoginPage.php');
-        }
-
-    }
-*/
 
 session_start();
 
